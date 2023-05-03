d0b6ea1b06d5b8c79063d1fa04b97f8e5a8b235d
Zihan Zhang <hblues419@gmail.com>
2023-05-03 02:48:03 +0100

    feat: implement distributed face detection and skip some frames

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
+import numpy as np
 from flask import Flask, render_template, request
-from flask_socketio import SocketIO
+from flask_socketio import SocketIO, emit
 import cv2
 from FaceRecognizer import FaceRecognizer
 from threading import Thread
@@ -7,50 +8,49 @@ import base64
 
 app = Flask(__name__)
 socketio = SocketIO(app)
-
-stop_camera_feed = False
+connections = {}
+data_uri_queue = {}
 
 # Initialize the face recognizer with known faces
 face_recognizer = FaceRecognizer('./known_faces.txt')
 
-@app.route('/')
-def index():
-    return render_template('index.html')
+def handle_camera_feed(sid):
+    while sid in connections:
+        if len(data_uri_queue[sid]) > 0:
+            data_uri = data_uri_queue[sid].pop(0)
+            handle_origin_img(sid, data_uri)
+
+def handle_origin_img(sid, data_uri):
+    data_bytes = base64.b64decode(data_uri.split(',')[1])
+    np_arr = np.frombuffer(data_bytes, np.uint8)
+    frame = cv2.imdecode(np_arr, cv2.IMREAD_COLOR)
+    image_bytes = face_recognizer.recognize_faces(frame)
+    image_base64 = base64.b64encode(image_bytes).decode('utf-8')
+    socketio.emit('detectedImage', image_base64, room=sid)
 
 @socketio.on('connect')
 def handle_connect():
-    # Create a new thread for each client to handle the camera feed
-    thread = Thread(target=handle_camera_feed)
+    sid = request.sid
+    connections[sid] = True
+    data_uri_queue[sid] = []
+    thread = Thread(target=handle_camera_feed, args=(sid,))
     thread.start()
+
+@socketio.on('originImage')
+def handle_origin_img_event(data_uri):
+    sid = request.sid
+    data_uri_queue[sid].append(data_uri)
+
 
 @socketio.on('disconnect')
 def handle_disconnect():
-    # Set a flag to stop the camera feed thread
-    global stop_camera_feed
-    stop_camera_feed = True
-    print("disconnect")
+    # Remove the disconnected client's connection from our dictionary
+    connections.pop(request.sid, None)
+    print("disconnect: " + str(request.sid))
 
-def handle_camera_feed():
-    global stop_camera_feed
-    stop_camera_feed = False
-
-    # Initialize the camera
-    camera = cv2.VideoCapture(0)
-
-    while not stop_camera_feed:
-        success, frame = camera.read()
-        if not success:
-            break
-        else:
-            # Recognize faces in the current frame
-            image_bytes = face_recognizer.recognize_faces(frame)
-            image_base64 = base64.b64encode(image_bytes).decode('utf-8')
-            # Send the image bytes to the client
-            socketio.emit('image', image_base64)
-
-    # Release the camera when we're done
-    camera.release()
-
+@app.route('/')
+def index():
+    return render_template('index.html')
 
 if __name__ == '__main__':
     socketio.run(app, debug=True)
